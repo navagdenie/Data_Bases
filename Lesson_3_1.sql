5a5e97b6525aa50897d608e2546260c0bdb1ffc1
navagdenie <vetlugnat@mail.ru>
2019-07-12 17:47:58 +0300

    Тема 3 Задание 1

--- Lesson_3_1.sql
+++ Lesson_3_1.sql
@@ -2,5 +2,6 @@
 потому что мне было важно сохранить значение в колонке,
 если например не заполнена только одна из двух дат*/
 
-UPDATE users SET created_at = NOW() WHERE created_at IS NULL or created_at = '1900-01-01 00:00';
-UPDATE users SET updated_at = NOW() WHERE updated_at IS NULL or updated_at = '1900-01-01 00:00';
+UPDATE users SET
+	created_at = IF(created_at IS NULL OR created_at = '0001-01-01 00:00:01', NOW(), created_at),
+	updated_at = IF(updated_at IS NULL OR updated_at = '0001-01-01 00:00:01', NOW(), updated_at)
