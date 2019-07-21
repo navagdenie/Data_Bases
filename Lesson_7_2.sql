24de66a2e59c01e73d78fa4040b47fa498f2278a
navagdenie <vetlugnat@mail.ru>
2019-07-21 20:58:37 +0300

    Тема 8 Задание 2

---
+++ Lesson_7_2.sql
@@ -0,0 +1,19 @@
+DROP TRIGGER IF EXISTS check_products_insert;
+
+CREATE TRIGGER check_products_insert BEFORE INSERT ON products
+FOR EACH ROW
+BEGIN
+	IF (NEW.name IS NULL AND NEW.description IS NULL) THEN 
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'INSERT canceled';
+	END IF;
+END
+
+DROP TRIGGER IF EXISTS check_products_update;
+
+CREATE TRIGGER check_products_update BEFORE UPDATE ON products
+FOR EACH ROW
+BEGIN
+	IF (NEW.name IS NULL AND NEW.description IS NULL) THEN 
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'UPDATE canceled';
+	END IF;
+END
