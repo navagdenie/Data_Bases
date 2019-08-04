63ea47700906d02862ed733580c02c5543b782b5
navagdenie <vetlugnat@mail.ru>
2019-08-04 19:23:56 +0300

    Исправила имена атрибутов

--- Query_Reviewes.sql
+++ Query_Reviewes.sql
@@ -2,7 +2,7 @@
 SELECT 
 	h.name hotel_name, 	
 	rm.name room_name,
-	nt.name nutricion,
+	nt.name nutrition,
 	b.date_from, 
 	b.date_to,
 	u.login username, 
@@ -20,5 +20,5 @@ FROM reviews r
 	LEFT JOIN users u ON r.user_id = u.id
 	LEFT JOIN bookings b ON r.booking_id = b.id
 	LEFT JOIN rooms rm ON b.room_id = rm.id
-	LEFT JOIN nutricion_types nt ON b.nutricion_type_id = nt.id
+	LEFT JOIN nutrition_types nt ON b.nutrition_type_id = nt.id
 WHERE h.id = %hotel_id%
